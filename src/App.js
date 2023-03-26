540a66b3a5ae2e24132d284ab8442a9b223cd5f1
Harsh-Fit <harshfitness24@gmail.com>
2023-03-26 11:50:23 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import PrivateComp from "./Components/PrivateComp";
+import CarListing from "./Components/CarListing";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import Booking from "./Components/Booking";
+import Driver from "./Components/Driver";
+import ATMcard from "./Components/ATM";
+import MyOrder from "./Components/MyOrder";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route element={<PrivateComp />}>
+            <Route path="/" element={<CarListing />} />
+            <Route path="/book-car/:id" element={<Booking />} />
+            <Route path="/driver" element={<Driver />} />
+            <Route path="ATM" element={<ATMcard />} />
+            <Route path="ticket-details/:email" element={<MyOrder />} />
+          </Route>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
